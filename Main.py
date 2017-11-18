eafebc1c71ca6261835eae172e639a192dccb038
Miguel Frias <miguelfrias@MacBook-Pro-de-Miguel.local>
2017-11-17 16:33:25 -0800

    Keep working

--- Main.py
+++ Main.py
@@ -20,32 +20,38 @@ lista_taquero_otros = Queue()
 ordenes = []
 
 def Atender(cliente):
-    for orden in cliente.getOrdenes():
-        orden.ready=True
+	for orden in cliente.getOrdenes():
+		orden.ready=True
 
 def AgregandoClientes(lista,taqueria,clientes):
-    for i in range(len(lista)):
-        customer = Cliente(lista[i]["datetime"],lista[i]["request_id"],lista[i]["orden"])
-        clientes.append(customer)
+	for i in range(len(lista)):
+        	customer = Cliente(lista[i]["datetime"],lista[i]["request_id"],lista[i]["orden"])
+        	clientes.append(customer)
 	for orden in customer.getOrdenes():
-            ordenes.append(orden)
-            ordenes_taqueria.put(orden)
-        taqueria.addCliente()
+		ordenes.append(orden)
+		ordenes_taqueria.put(orden)
+	taqueria.addCliente()
+
 def getData():
-    try:
-        sqs = boto3.client('sqs')
-        data = Recieve_Orders(sqs)
-    except:
-        data = Take_Orders(inbound_Order)
-        ordenes_aws.append(data)
+	    inbound_Order = str({"datetime": "2017-01-01 23:23:23", "request_id": "123-$
+                   "orden": [ { "part_id": "123-111",  "type": "taco", "meat": $
+                              { "part_id": "123-222", "type": "mulita", "meat":$
+                              { "part_id": "123-333", "type": "quesadilla", "me$
+
+	try:
+		sqs = boto3.client('sqs')
+		data = Recieve_Orders(sqs)
+	except:
+		data = Take_Orders(inbound_Order)
+		ordenes_aws.append(data)
 
 def main():
     start = tiempo()
 
-    inbound_Order = str({"datetime": "2017-01-01 23:23:23", "request_id": "123-123-123",
-                   "orden": [ { "part_id": "123-111",  "type": "taco", "meat": "asada", "quantity": 3, "ingredients": [ "cebolla", "salsa"] },
-                              { "part_id": "123-222", "type": "mulita", "meat": "asada", "quantity": 1, "ingredients": []  },
-                              { "part_id": "123-333", "type": "quesadilla", "meat": "adobada", "quantity": 2, "ingredients": ["cebolla", "aguacate", "salsa"]} ]})
+#    inbound_Order = str({"datetime": "2017-01-01 23:23:23", "request_id": "123-123-123",
+#                   "orden": [ { "part_id": "123-111",  "type": "taco", "meat": "asada", "quantity": 3, "ingredients": [ "cebolla", "salsa"] },
+#                              { "part_id": "123-222", "type": "mulita", "meat": "asada", "quantity": 1, "ingredients": []  },
+#                              { "part_id": "123-333", "type": "quesadilla", "meat": "adobada", "quantity": 2, "ingredients": ["cebolla", "aguacate", "salsa"]} ]})
 
     ordenes_aws=[]
     clientes = []
