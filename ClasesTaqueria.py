1b1f851cdf38a9f0527af325be129b0cc3bdb47e
GraceFarrell <nataliab@cetys.edu.mx>
2017-11-23 16:46:39 -0800

    Agregue Tortilleras

--- ClasesTaqueria.py
+++ ClasesTaqueria.py
@@ -7,7 +7,7 @@ class Taquero:
 		self.low_priority = Queue()
 		self.min_priority = Queue()
 		self.waiting = Queue()
-		self.ingredientes = {"cebolla":500,"salsa":500,"cilantro":500,"frijoles":500,"aguacate":500}
+		self.ingredientes = {"cebolla":500,"salsa":500,"cilantro":500,"frijoles":500,"aguacate":500,"tortillas":500}
 
 class Taqueria:
     	def __init__(self):
@@ -29,6 +29,7 @@ class Orden:
 		self.toPrepare = self.quantity
 		self.time_by_type = self.setTimeByType()
 		self.current_total_time = self.time_by_type * self.toPrepare
+		self.steps = []
 	def getType(self):
 		return self.typee	
 	def getMeat(self):
@@ -54,36 +55,44 @@ class Orden:
 		return "part_id:{0}\n typee:{1}\n meat:{2}\n quantity:{3}\n ingredients:{4}\n".format(self.part_id,self.typee,self.meat,self.quantity,self.ingredients)
 	def __iter__(self):
 		return self
+	def getSteps(self):
+		for step in self.steps:
+			print(step)
+		
 
     
 class Cliente:
-    	def __init__(self,date,idd,ordenes):
-        	self.date=date
-        	self.idd=idd
-        	self.numero_ordenes = 0
-        	self.ordenes = self.addOrden(ordenes)
+	def __init__(self,date,idd,ordenes):
+		self.date=date
+		self.idd=idd
+		self.numero_ordenes = 0
+		self.ordenes = self.addOrden(ordenes)
         
-    	def addOrden(self,orden):
-        	ordenes = []
-        	for i in range (len(orden)):
-        	    no = Orden(orden[i]["part_id"],orden[i]["type"],orden[i]["meat"],orden[i]["quantity"],orden[i]["ingredients"])
-        	    self.numero_ordenes += 1
-        	    ordenes.append(no)
-        	return ordenes
-    	def getOrdenesSize(self):
-        	return self.numero_ordenes
-    	def getOrdenes(self):
-        	return self.ordenes
-    	def getCompletado(self):
-        	temp = 0
-        	for orden in self.ordenes:
-        	    if orden.ready:
-        	        temp += 1
-        	if temp == self.numero_ordenes:
-        	    return True
-        	return False
-    	def __str__(self):
-        	temp = ""
-        	for orden in self.ordenes:
-        	    temp += orden.String()
-        	return "Cliente: {0} \n{1}\n{2}".format(self.idd,self.date,temp)
+	def addOrden(self,orden):
+		ordenes = []
+		for i in range (len(orden)):
+			no = Orden(orden[i]["part_id"],orden[i]["type"],orden[i]["meat"],orden[i]["quantity"],orden[i]["ingredients"])
+			self.numero_ordenes += 1
+			ordenes.append(no)
+		return ordenes
+	def getOrdenesSize(self):
+		return self.numero_ordenes
+	def getOrdenes(self):
+		return self.ordenes
+	def getSteps(self):
+		for orden in self.ordenes:
+			for step in orden.steps:
+				print(step)
+	def getCompletado(self):
+		temp = 0
+		for orden in self.ordenes:
+			if orden.ready:
+				temp += 1
+		if temp == self.numero_ordenes:
+			return True
+		return False
+	def __str__(self):
+		temp = ""
+		for orden in self.ordenes:
+			temp += orden.String()
+		return "Cliente: {0} \n{1}\n{2}".format(self.idd,self.date,temp)
