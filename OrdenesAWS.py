14c5d00ba52cb1ee0786349a564ee69cf1797995
Miguel Frias <miguelfrias@MacBook-Pro-de-Miguel.local>
2017-11-29 20:00:06 -0800

    Final

--- OrdenesAWS.py
+++ OrdenesAWS.py
@@ -9,9 +9,6 @@ def Recieve_Orders():
 	sqs = boto3.client('sqs')
 	response = sqs.receive_message(QueueUrl = 'https://sqs.us-east-1.amazonaws.com/292274580527/cc406_team2')
 	
-#	message_string = ""
-
-#	for message in response["Messages"]:
 	message_string = response["Messages"][0]['Body']
 	receipt = response["Messages"][0]["ReceiptHandle"]
 
